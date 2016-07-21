abf5d4815dba8f77f5745371d6435c8b477f61a6
helder <helderwmarcos@gmail.com>
2016-07-20 22:48:10 -0300

    Second

--- src/CodeCategory/Models/Category.php
+++ src/CodeCategory/Models/Category.php
@@ -41,7 +41,7 @@ class Category extends Model implements SluggableInterface
 
     public function posts()
     {
-        return $this->morphedByMany('\CodePress\CodeCategory\Models\Post', 'categorizable', 'codepress_categorizables');
+        return $this->morphedByMany('CodePress\CodeCategory\Models\Post', 'categorizable', 'codepress_categorizables');
     }
 
     public function parent()
